e7e23e9df075ac383814aa44de29a760bc32cffe
mavisclub <mavisclub@gmail.com>
2025-07-14 20:57:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello Vue!</h1>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
